de4280ced67b5bcfbdb8ec694503b68ab9a36dc0
idk <problemsolver@openmailbox.org>
2017-09-26 23:28:01 -0400

    I can't tell if debootstrap is using the caching proxy

--- config.mk
+++ config.mk
@@ -4,7 +4,7 @@ export KEY ?= "70D2060738BEF80523ACAFF7D75C03B39B5E14E1"
 export proxy_host ?= 192.168.1.98
 export proxy_port ?= 3142
 
-export proxy_addr ?= http://$(proxy_host):$(proxy_port)
+export proxy_addr ?= http://$(proxy_host):$(proxy_port)/
 
 #export distro ?= debian
 # Only Ever
@@ -18,9 +18,9 @@ export distro ?= devuan
 export custom ?= yes
 #export nonfree ?= yes
 #export server ?= yes
-export mirror_debian ?= $(proxy_addr)/ftp.us.debian.org/debian
-export mirror_devuan ?= $(proxy_addr)/us.mirror.devuan.org/merged
-export mirror_ubuntu ?= $(proxy_addr)/archive.ubuntu.com/ubuntu
+export mirror_debian ?= http://$(proxy_host):$(proxy_port)ftp.us.debian.org/debian
+export mirror_devuan ?= http://$(proxy_host):$(proxy_port)us.mirror.devuan.org/merged
+export mirror_ubuntu ?= http://$(proxy_host):$(proxy_port)archive.ubuntu.com/ubuntu
 
 getname:
 	@echo "$(image_prename)-$(distro)"
