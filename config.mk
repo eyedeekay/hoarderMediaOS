3d3ab7aecf7d1819bf69f69868e44ac65327a536
idk <problemsolver@openmailbox.org>
2017-12-03 03:08:38 -0500

    switch to all conditional sets in config.mk

--- config.mk
+++ config.mk
@@ -1,11 +1,11 @@
-export image_prename = tv
-export KEY = "70D2060738BEF80523ACAFF7D75C03B39B5E14E1"
+export image_prename ?= tv
+export KEY ?= "70D2060738BEF80523ACAFF7D75C03B39B5E14E1"
 
 #export proxy_host = 192.168.1.98
 export proxy_host ?= 172.17.0.2
-export proxy_port = 3142/
+export proxy_port ?= 3142/
 
-export proxy_addr = http://$(proxy_host):$(proxy_port)
+export proxy_addr ?= http://$(proxy_host):$(proxy_port)
 
 #export distro ?= debian
 # Only Ever
@@ -23,11 +23,11 @@ export keyserver = hkp://p80.pool.sks-keyservers.net:80
 #export keyserver = http://pool.sks-keyservers.net:11371/
 
 #These must equal "yes" all lower case to be selected
-export hardened = no
-export custom = yes
-export nonfree = no
-export server = no
+export hardened ?= no
+export custom ?= yes
+export nonfree ?= no
+export server ?= no
 
-export mirror_debian = http://ftp.us.debian.org/debian
-export mirror_devuan = http://us.mirror.devuan.org/merged
-export mirror_ubuntu = http://archive.ubuntu.com/ubuntu
+export mirror_debian ?= http://ftp.us.debian.org/debian
+export mirror_devuan ?= http://us.mirror.devuan.org/merged
+export mirror_ubuntu ?= http://archive.ubuntu.com/ubuntu
