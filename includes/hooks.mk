920c39b64401e2f36c2215a8a6765b6bb5fc085a
idk <problemsolver@openmailbox.org>
2018-01-09 16:14:03 -0500

    added hooks

--- includes/hooks.mk
+++ includes/hooks.mk
@@ -2,9 +2,9 @@ define DOCKER_HOOKS
 	docker pull debian:sid
 	docker pull debitux/devuan:unstable
 	docker pull alpine:3.6
-	#docker pull eyedeekay/osint_complex:nmap-vulners
-	#docker pull eyedeekay/osint_complex:OSRFramework
-	#docker pull eyedeekay/osint_complex:theHarvester
+	docker pull eyedeekay/osint_complex:nmap-vulners
+	docker pull eyedeekay/osint_complex:OSRFramework
+	docker pull eyedeekay/osint_complex:theHarvester
 	#docker pull hoardermediaos/lb-build-$(distro)
 	#docker pull nagev/tor
 endef
@@ -13,6 +13,7 @@ export DOCKER_HOOKS
 
 docker-hooks:
 	echo "$$DOCKER_HOOKS" config/hooks/docker.hook.binary
+	echo "$$DOCKER_HOOKS" config/hooks/docker.hook.chroot
 
 docker-commands:
 	echo 'docker run -d --name tor_instance -p 127.0.1.1:9150:9150 nagev/tor'
